e7d5878104c2e888a3422a4729274f341663c9b1
RiccardoVoltolina <riky.volto@live.com>
2024-01-17 17:08:48 +0100

    print all cards on page

--- yougiohApi/src/App.jsx
+++ yougiohApi/src/App.jsx
@@ -10,14 +10,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 function App() {
 
   const [cards, setCards] = useState([])
-
-  const [text, setText] = useState('ciao')
-
-
-  function changeText() {
-    setText('pippo')
-  };
-
+  
   const getCards = () => {
     axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
       .then(response => {
@@ -32,17 +25,26 @@ function App() {
 
   return (
     <>
-      <button onClick={getCards}>cliccami</button>
-     
+      <div className="container">
+        <button onClick={getCards}>cliccami</button>
+        <div className="row">
+          {cards.map((card) => (
+            <div className="col-4 mb-4" key={card.id}>
+              <div className='card h-100 d-flex flex-column'>
+                <h1 className='text-center'>{card.archetype}</h1>
+                {card.card_images.map((cardImage) => (
+                  <img key={cardImage.id} src={cardImage.image_url_cropped} className="card-img-top" alt="" />
+                ))}
+                <div className="card-body flex-grow-1">
+                  <h5 className="card-title">{card.name}</h5>
+                  <p className="card-text">{card.desc}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
 
-      {cards.map((card) => {
-        return (
-
-          <div className='card' key={card.id}>
-            {card.name}
-          </div>
-        )
-      })}
 
     </>
   )
