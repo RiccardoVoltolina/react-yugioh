765c918d33ce4805011405c5cedce4f3476eb765
RiccardoVoltolina <riky.volto@live.com>
2024-01-17 10:49:46 +0100

    install axios

--- yougiohApi/src/App.jsx
+++ yougiohApi/src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import axios, {isCancel, AxiosError} from 'axios';
+
+// https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0
 
 function App() {
+  axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+  
 
   return (
     <>
