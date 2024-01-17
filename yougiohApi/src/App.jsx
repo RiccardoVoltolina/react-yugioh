e6417cdecee3ecb72d95bc63eb1d4c3c2af381f7
RiccardoVoltolina <riky.volto@live.com>
2024-01-17 12:41:30 +0100

    print first value on page

--- yougiohApi/src/App.jsx
+++ yougiohApi/src/App.jsx
@@ -1,25 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import axios, {isCancel, AxiosError} from 'axios';
+import axios, { isCancel, AxiosError } from 'axios';
 
-// https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0
+
+
+
 
 function App() {
-  axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
-  
 
+  // const [allCards, setAllCards] = useState('');
+
+  const [cards, setCards] = useState('')
+
+  const getCards = () => {
+    axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+     .then(response => {
+      console.log(response.data.data);
+
+      setCards(response.data.data)
+     }).catch(error=>{
+      console.log(error);
+     })
+  }
+
+  // useEffect(() => {
+  //   axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then((response) => {
+  //     console.log(response.data.data);
+
+  //     setAllCards(response.data.data)
+
+
+      
+  //     console.log(allCards);
+  //   });
+
+  // }, []);
+  // if (!allCards) return null;
   return (
     <>
-      <h1>ciao</h1>
+      <button onClick={getCards}>cliccami</button>
+      {cards ? <p>{cards}</p> : null}
     </>
   )
 }
